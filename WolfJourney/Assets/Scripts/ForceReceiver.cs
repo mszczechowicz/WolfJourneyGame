cf9f6fb0c1e883f39cb86d5871b5a0f006768635
mszczechowicz <scela0718@gmail.com>
2023-06-01 22:03:19 +0200

    Set GymScene for World building

--- WolfJourney/Assets/Scripts/ForceReceiver.cs
+++ WolfJourney/Assets/Scripts/ForceReceiver.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.AI;
 
--- WolfJourney/Assets/Scripts/StateMachines/Player/PlayerFreeLookState.cs
+++ WolfJourney/Assets/Scripts/StateMachines/Player/PlayerFreeLookState.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerFreeLookState : PlayerBaseState
